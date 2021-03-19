22e8e4dbbbe9372a5f95ad2d2680f062fa16c2ba
Alison Lee <alison2686@gmail.com>
2021-03-18 18:51:28 -0700

    Weather app version1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
-// import Weather from './components/weather';
-import CardExampleCard from './components/weather'
+import Weather from './components/weather'
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 export default function App() {
   
   const [lat, setLat] = useState([])
   const [long, setLong] = useState([])
   const [weatherData, setWeatherData] = useState([])
-  // const [error, setError] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,46 +30,16 @@ export default function App() {
     fetchData();
   }, [lat,long])
 
-  // function handleResponse(response) {
-  //   if (response.ok) {
-  //     return response.json()
-  //   } else {
-  //     throw new Error ("Please Enable your Location in your browser")
-  //   }
-  // }
-
-  // function getWeather(lat, long) {
-  //   return fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-  //   .then(res => res.json())
-  //   .then(result => {
-  //     weatherData(result)
-  //     console.log(result);
-  //   });
-  // }
-
-  // function mapDataToWeatherInterface(data) {
-  //   const mapped = {
-  //     date: data.dt * 1000, // convert from seconds to milliseconds
-  //     description: data.weather[0].main,
-  //     temperature: Math.round(data.main.temp),
-  //   };
-  
-  //   // Add extra properties for the five day forecast: dt_txt, icon, min, max
-  //   if (data.dt_txt) {
-  //     mapped.dt_txt = data.dt_txt;
-  //   }
-  
-  //   return mapped;
-  // }
-
-
-  
   return (
     <div className="App">
       {(typeof weatherData.main != 'undefined') ? (
-        <CardExampleCard weatherData={weatherData}/>
+        <Weather weatherData={weatherData}/>
       ): (
-        <div></div>
+        <div>
+          <Dimmer active>
+            <Loader>Loading...</Loader>
+          </Dimmer>
+        </div>
       )}
       
     </div>
